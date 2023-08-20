7e40898607b0991e9794b8ffbbd23bfd4a2cc859
Sankugarud <sanketgarud1998@gmail.com>
2023-08-21 01:09:43 +0530

    submitting code-Mon Aug 21 2023 01:09:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,32 @@
-
-import React , {useState} from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const [modal, setModal] = useState(false);
 
-  let showmodal = () => {
+  const showModal = () => {
     setModal(true);
   }
 
   return (
     <div>
-        <div className="parent">
-          <h1>Parent Component</h1>
-          <div className="child">
-            <h3>Child Component</h3> 
-            <button onClick={showmodal}>Show Modal</button>
-
-           
-          </div>
-          <div className="modal">
-          {
-            modal && (
-              <div>
-              <h3>Modal content</h3>
-              <p>This is modal content</p>
-            </div>
-            )
-          }
-          </div>
-
+      <div className="parent">
+        <h1>Parent Component</h1>
+        <div className="child">
+          <h3>Child Component</h3> 
+          <button onClick={showModal}>Show Modal</button>
         </div>
+        <div className="modal">
+          {modal && (
+            <div>
+              <h3>Modal Content</h3>
+              <p>This is the modal content.</p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
